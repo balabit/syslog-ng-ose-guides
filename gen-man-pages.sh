4b65dae2756d5020189225f16a83b0cd71c901b0
Robert Fekete <frobert@writer.(none)>
2009-11-30 16:18:00 +0100

    Manpage-related updates

--- gen-man-pages.sh
+++ gen-man-pages.sh
@@ -4,10 +4,13 @@ srcdir=$1
 #shift
 for manpage in $1; do
 	manxml="${srcdir}/${manpage}.xml"
+	#FIXME th script does not get the filename right for some reason
+	echo $manxml
+	echo $manpage
 	output="out/${manpage}"
 	if [ -f ${manxml} ]; then
 		echo "Generating $manpage from $manxml $output"
-		xsltproc --xinclude --output $output xml-stylesheet/pdf/docbook-xsl-1.73.2/manpages/docbook.xsl $manxml
+		xsltproc --xinclude --output $output xml-stylesheet/pdf/docbook-xslt/manpages/docbook.xsl $manxml
 	else
 		echo "XML source not found for $manpage"
 	fi
