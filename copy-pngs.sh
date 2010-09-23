b22bc12eedab5aa851c2acf512eb03ce3824e347
Robert Fekete <frobert@balabit.com>
2010-09-23 10:24:05 +0200

    [html] Image copying corrections

--- copy-pngs.sh
+++ copy-pngs.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-imgroot=http://share.balabit/tt/images
-GET="wget --no-directories -r -A.gif,.png,.html --output-file=wget.log --directory-prefix=./$1"
-
-###Copying common graphics elements (BalaBit logo, admonition icons, css etc.)
-echo Processing admonitions
-cp xml-stylesheet/html/balabit.css $1
-cp xml-stylesheet/pdf/arculat/BB_logo_trnsp_white.png $1
-cp xml-stylesheet/pdf/arculat/admon/example_html.png $1/example.png
-cp xml-stylesheet/pdf/arculat/admon/note_html.png $1/note.png
-cp xml-stylesheet/pdf/arculat/admon/tip_html.png $1/tip.png
-cp xml-stylesheet/pdf/arculat/admon/warning_html.png $1/warning.png
+. ./xml-stylesheet/scripts/copy-admons.sh
 
 case "$1" in 
 
@@ -30,3 +20,4 @@ case "$1" in
 	;;
 esac
 
+. xml-stylesheet/scripts/display-wget-errors.sh
