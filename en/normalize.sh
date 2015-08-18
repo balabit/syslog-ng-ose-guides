3f5749296ff9d20b165f426ed3f3f910ef12b85e
Robert Fekete <robert.fekete@balabit.com>
2015-08-18 12:43:08 +0200

    [ose] Deletes unneeded files from the repo, related to DOC-2626

--- en/normalize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-#for f in `ls chapters/*.xml | grep -v admin-guide.xml`
-#do
-#	echo $f
-#	/usr/bin/tidy -xml -utf8 -omit -i -w 78 $f | grep -v "<?xml-stylesheet href" | grep -v "DOCTYPE chapter SYSTEM" > $f.new
-#	mv $f.new $f
-#done
